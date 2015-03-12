746a4dcf9afc919f58b12036b2f9b04c49eb981a
Nachiket Kakatkar <nachiketkakatkar@gmail.com>
2015-03-13 02:34:44 +0530

    Permutation & filtering both working

--- permute.js
+++ permute.js
@@ -1,7 +1,3 @@
-function permute(s) {
-  return findPermutations(s, {});
-}
-
 function findPermutations(s, wordsStore) {
   //console.log('s = ' + s);
   if(wordsStore[s]) {
@@ -18,17 +14,28 @@ function findPermutations(s, wordsStore) {
   for(var i = 0; i < len; i++) {
     var currentHead = s[i];
     var subStr = s.slice(0, i) + s.slice(i + 1, len);
-    var tmpOp = findPermutations(subStr, wordsStore), tmpOpLen = tmpOp.length;
-    wordsStore[subStr] = tmpOp;
-    for(var j = 0; j < tmpOpLen; j++) {
-
-      res.push(currentHead + "" + tmpOp[j]);
+    var tmpOutput = findPermutations(subStr, wordsStore), tmpOutputLen = tmpOutput.length;
+    wordsStore[subStr] = tmpOutput;
+    for(var j = 0; j < tmpOutputLen; j++) {
+      res.push(currentHead + "" + tmpOutput[j]);
     }
   }
   //console.log('res ', res);
   return res;
 }
 
-var input = "abcd";
-var output = permute(input);
+function permute(s, filter) {
+  var p = findPermutations(s, {}),
+    res = [];
+  for(var i = 0; i < p.length; i++) {
+    if(p[i].indexOf(filter) >= 0){
+      res.push(p[i]);
+    }
+  }
+  return res;
+}
+
+var input = "edcba";
+var filter = "bc";
+var output = permute(input, filter);
 console.log(output);
